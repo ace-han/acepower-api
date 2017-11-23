de3193782c75deed048056f7b1f2657245c197b0
Ace <ace.jl.han@gmail.com>
2017-11-24 01:42:18 +0800

    mark any `Pending` orders in an appropriate timedelta as `Timeout` in celery tasks

---
+++ oscarx/order/tasks.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+from datetime import timedelta
+
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.conf import settings
+from django.utils import timezone
+from oscar.core.loading import get_model
+from django.db.transaction import atomic
+
+
+Order = get_model('order', 'Order')
+
+@atomic
+@shared_task(bind=True)
+def mark_as_timeout(self):
+    '''
+    mark order status as Timeout and cancel the allocation of the each stock
+    '''
+    logger = get_task_logger(self.name)
+    now = timezone.now()
+    delta = timedelta(seconds=settings.OSCAR_ORDER_TIMEOUT_SEC)
+    fine_time = now - delta
+    o_qs = Order.objects.filter(status='Pending', date_placed__lt=fine_time)
+    if not o_qs.exists():
+        logger.info('No `Pending` orders need mark as `Timeout`')
+        return
+    
+    o_counter = 0
+    l_counter = 0
+    for o in o_qs:
+        for line in o.lines.all().select_related('stockrecord'):
+            if line.stockrecord:
+                line.stockrecord.cancel_allocation(line.quantity)
+                l_counter += 1
+        o.set_status('Timeout')
+        o_counter += 1
+    logger.info('Pending order size: %s, order line size: %s', o_counter, l_counter)
